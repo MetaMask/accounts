944590785d389e694d061827ca682d06786c5bc9
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-09-18 13:01:17 +0200

    Update keyring-eth-hd tests

--- packages/keyring-eth-hd/src/index.js
+++ packages/keyring-eth-hd/src/index.js
@@ -14,7 +14,12 @@ import {
   signTypedData,
   SignTypedDataVersion,
 } from '@metamask/eth-sig-util';
-import bip39 from '@metamask/scure-bip39';
+import {
+  generateMnemonic,
+  // eslint-disable-next-line n/no-sync
+  mnemonicToSeedSync,
+  validateMnemonic,
+} from '@metamask/scure-bip39';
 import { wordlist } from '@metamask/scure-bip39/dist/wordlists/english';
 import { assertIsHexString, remove0x } from '@metamask/utils';
 import { HDKey } from 'ethereum-cryptography/hdkey';
@@ -34,7 +39,7 @@ class HdKeyring {
   }
 
   generateRandomMnemonic() {
-    this._initFromMnemonic(bip39.generateMnemonic(wordlist));
+    this._initFromMnemonic(generateMnemonic(wordlist));
   }
 
   _uint8ArrayToString(mnemonic) {
@@ -287,7 +292,7 @@ class HdKeyring {
     this.mnemonic = this._mnemonicToUint8Array(mnemonic);
 
     // validate before initializing
-    const isValid = bip39.validateMnemonic(this.mnemonic, wordlist);
+    const isValid = validateMnemonic(this.mnemonic, wordlist);
     if (!isValid) {
       throw new Error(
         'Eth-Hd-Keyring: Invalid secret recovery phrase provided',
@@ -295,7 +300,7 @@ class HdKeyring {
     }
 
     // eslint-disable-next-line n/no-sync
-    const seed = bip39.mnemonicToSeedSync(this.mnemonic, wordlist);
+    const seed = mnemonicToSeedSync(this.mnemonic, wordlist);
     this.hdWallet = HDKey.fromMasterSeed(seed);
     this.root = this.hdWallet.derive(this.hdPath);
   }
