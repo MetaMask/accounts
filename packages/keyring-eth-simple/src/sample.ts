1c355901e343f145751560c5e383dc9b5329978b
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2024-09-27 09:59:02 +0200

    chore: fix more linting errors

--- packages/keyring-eth-simple/src/sample.ts
+++ packages/keyring-eth-simple/src/sample.ts
@@ -1,4 +1,5 @@
-import SimpleKeyring from '.';
+// eslint-disable-next-line @typescript-eslint/naming-convention
+import SimpleKeyring from './simple-keyring';
 
 const keyring = new SimpleKeyring();
 
