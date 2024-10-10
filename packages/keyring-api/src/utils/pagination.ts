dd7f43fe4388cc20c21ee72ecdd6c95f4944e6f4
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2024-10-10 13:03:10 +0200

    fix: don't use union in pagination

--- packages/keyring-api/src/utils/pagination.ts
+++ packages/keyring-api/src/utils/pagination.ts
@@ -1,19 +1,17 @@
 import type { Infer } from '@metamask/superstruct';
-import { number, string, union, object } from '@metamask/superstruct';
+import { number, string } from '@metamask/superstruct';
+import { exactOptional, object } from '@metamask/utils';
 
-export const PaginationStruct = union([
-  object({
-    /**
-     * Maximum number of items to return.
-     */
-    limit: number(),
-  }),
-  object({
-    /**
-     * Next cursor to iterate over the results.
-     */
-    next: string(),
-  }),
-]);
+export const PaginationStruct = object({
+  /**
+   * Maximum number of items to return.
+   */
+  limit: number(),
+
+  /**
+   * Next cursor to iterate over the results.
+   */
+  next: exactOptional(string()),
+});
 
 export type Pagination = Infer<typeof PaginationStruct>;
