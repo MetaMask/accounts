bece00ec97736a6c7c99c1c383ef013150fd6330
Charly Chevalier <charly.chevalier@consensys.net>
2024-10-16 12:37:22 +0200

    chore(keyring-eth-ledger-bridge): lower coverage for branches (90.64 -> 90.14)

--- packages/keyring-eth-ledger-bridge/jest.config.js
+++ packages/keyring-eth-ledger-bridge/jest.config.js
@@ -23,7 +23,7 @@ module.exports = merge(baseConfig, {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 90.64,
+      branches: 90.14,
       functions: 95.95,
       lines: 94.76,
       statements: 94.81,
