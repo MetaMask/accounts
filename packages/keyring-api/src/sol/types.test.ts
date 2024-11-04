a631c8f501e5ab762528c363d9050ac0df0cff6d
Antonio Regadas <antonio.regadas@consensys.net>
2024-11-04 12:14:49 +0000

    chore: prettier and lint

--- packages/keyring-api/src/sol/types.test.ts
+++ packages/keyring-api/src/sol/types.test.ts
@@ -20,7 +20,9 @@ describe('types', () => {
       '0x1234',
       'not-an-address',
     ])('rejects invalid address; %s', (address) => {
-      expect(() => SolAddressStruct.assert(address)).toThrow();
+      expect(() => SolAddressStruct.assert(address)).toThrow(
+        `Expected a value of type \`SolAddress\`, but received: \`"${address}"\``,
+      );
     });
   });
 });
