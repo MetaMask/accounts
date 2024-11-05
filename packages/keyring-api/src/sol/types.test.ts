126cb763789908f597aa957421d9aea17ab9f3f0
António Regadas <apregadas@gmail.com>
2024-11-05 09:32:10 +0000

    Update packages/keyring-api/src/sol/types.test.ts
    Co-authored-by: Charly Chevalier <charly.chevalier@consensys.net>

--- packages/keyring-api/src/sol/types.test.ts
+++ packages/keyring-api/src/sol/types.test.ts
@@ -5,7 +5,7 @@ describe('types', () => {
     it.each([
       '7EcDhSYGxXyscszYEp35KHN8vvw3svAuLKTzXwCFLtV',
       'A5R99q8qeyUhgwYAdp5h8pAD1iteVjCzpCv7G6JRZLaQ',
-    ])('is valid address; %s', (address) => {
+    ])('is valid address: %s', (address) => {
       expect(() => SolAddressStruct.assert(address)).not.toThrow();
     });
 
