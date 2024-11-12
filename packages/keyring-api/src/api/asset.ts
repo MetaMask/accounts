96ecdddb8e5d96f99b107f7c4d7bdde9e54b0915
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2024-11-12 22:19:04 +0100

    chore: remove unused types

--- packages/keyring-api/src/api/asset.ts
+++ packages/keyring-api/src/api/asset.ts
@@ -1,9 +1,7 @@
-import type { Infer } from '@metamask/superstruct';
-import { literal, string, union } from '@metamask/superstruct';
+import { literal } from '@metamask/superstruct';
 import { CaipAssetIdStruct, CaipAssetTypeStruct } from '@metamask/utils';
 
 import { object } from '../superstruct';
-import { StringNumberStruct } from '../utils';
 
 /**
  * This struct represents an asset. The `fungible` property is used to tag the
@@ -45,57 +43,3 @@ export const NonFunginleAssetStruct = object({
    */
   id: CaipAssetIdStruct,
 });
-
-/**
- * This struct represents an asset, including the amount and unit if the asset
- * is fungible.
- *
- * The `fungible` property of the `asset` field is used to tag the union type
- * and allow the following pattern:
- *
- * ```ts
- * if (asset.fungible) {
- *   // Use asset.type
- * } else {
- *   // Use asset.id
- * }
- * ```
- *
- * @example
- * ```ts
- * fee: {
- *   asset: {
- *     fungible: true,
- *     type: 'eip155:1/slip44:60',
- *   },
- *   amount: '0.01',
- *   unit: 'ETH',
- * },
- * ```
- *
- * @example
- * ```ts
- * asset: {
- *   fungible: false,
- *   id: 'hedera:mainnet/nft:0.0.55492/12',
- * },
- * ```
- */
-export const AssetAmountStruct = union([
-  object({
-    asset: FunginleAssetStruct,
-
-    /**
-     * Amount of the asset.
-     */
-    amount: StringNumberStruct,
-
-    /**
-     * Asset unit.
-     */
-    unit: string(),
-  }),
-  object({
-    asset: NonFunginleAssetStruct,
-  }),
-]);
