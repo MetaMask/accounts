52b0d86153287ece8b96b9dee1941527bb7ac3c4
Charly Chevalier <charly.chevalier@consensys.net>
2024-11-12 11:02:39 +0100

    feat: adds Solana account type to keyring API (#93)
    We are adding the `SolAccountType` to support Solana account creation.
    
    Co-authored-by: António Regadas <apregadas@gmail.com>

--- packages/keyring-api/src/index.ts
+++ packages/keyring-api/src/index.ts
@@ -2,6 +2,7 @@ export * from './api';
 export * from './btc';
 export type * from './contexts';
 export * from './eth';
+export * from './sol';
 export * from './events';
 export * from './internal';
 export * from './KeyringClient';
