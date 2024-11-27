8be617b4e922c8efbaac1f4383abcb1f6e1827f8
Charly Chevalier <charly.chevalier@consensys.net>
2024-11-27 15:40:54 +0100

    test(keyring-internal-api): add tests for Keyring RPC methods

---
+++ packages/keyring-internal-api/src/rpc.test.ts
@@ -0,0 +1,11 @@
+import { KeyringRpcMethod, isKeyringRpcMethod } from './rpc';
+
+describe('isKeyringRpcMethod', () => {
+  it.each(Object.values(KeyringRpcMethod))('returns true for: KeyringRpcMethod.$s', (method) => {
+    expect(isKeyringRpcMethod(method)).toBe(true);
+  });
+
+  it('returns false for unknown method', () => {
+    expect(isKeyringRpcMethod('keyring_unknownMethod')).toBe(false);
+  });
+});
