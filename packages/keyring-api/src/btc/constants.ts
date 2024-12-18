470fa6778e26a0e80c8562bfe2f3d3ef4eb3812e
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2024-12-18 12:47:33 -0300

    chore: add BTC scopes
    Co-authored-by: Dario Anongba Varela <dario.anongba@gmail.com>

--- packages/keyring-api/src/btc/constants.ts
+++ packages/keyring-api/src/btc/constants.ts
@@ -7,4 +7,7 @@ export enum BtcScopes {
   Namespace = 'bip122',
   Mainnet = 'bip122:000000000019d6689c085ae165831e93',
   Testnet = 'bip122:000000000933ea01ad0ee984209779ba',
+  Testnet4 = 'bip122:00000000da84f2bafbbc53dee25a72ae',
+  Signet = 'bip122:00000008819873e925422c1ff0f99f7c',
+  Regtest = 'bip122:regtest',
 }
