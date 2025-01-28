eb05c91a199b90cc63db876ce175261a38ec1498
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2025-01-28 13:05:47 +0100

    chore: update error message

--- packages/keyring-api/src/api/account.test.ts
+++ packages/keyring-api/src/api/account.test.ts
@@ -52,7 +52,7 @@ describe('api', () => {
         scopes: [scope],
       };
       expect(() => assert(account, KeyringAccountStruct)).toThrow(
-        `At path: scopes.0 -- Expected the value to satisfy a union of \`string | string\`, but received: "${scope}"`,
+        `At path: scopes.0 -- Expected a string matching \`/^(?<namespace>[-a-z0-9]{3,8}):(?<reference>[-_a-zA-Z0-9]{1,32})$/\` but received "${scope}"`,
       );
     });
   });
