2b3ea95ed876c12681b11c250b00acfac43e91b1
Charly Chevalier <charly.chevalier@consensys.net>
2025-03-20 14:05:10 +0100

    refactor: move deprecated display* flags to Snap keyring events

--- packages/keyring-snap-bridge/src/events.ts
+++ packages/keyring-snap-bridge/src/events.ts
@@ -6,8 +6,8 @@ import {
   RequestRejectedEventStruct,
   KeyringAccountStruct,
 } from '@metamask/keyring-api';
-import { object } from '@metamask/keyring-utils';
-import { union } from '@metamask/superstruct';
+import { exactOptional, object } from '@metamask/keyring-utils';
+import { boolean, union } from '@metamask/superstruct';
 
 import { KeyringAccountV1Struct } from './account';
 
@@ -17,6 +17,12 @@ export const AccountCreatedEventStruct = object({
   params: object({
     ...OriginalAccountCreatedEventStruct.schema.params.schema,
     account: union([KeyringAccountV1Struct, KeyringAccountStruct]),
+
+    // Now deprecated, see `SnapKeyringInternalOptions` instead.
+    displayConfirmation: exactOptional(boolean()),
+
+    // Now deprecated, see `SnapKeyringInternalOptions` instead.
+    displayAccountNameSuggestion: exactOptional(boolean()),
   }),
 });
 
