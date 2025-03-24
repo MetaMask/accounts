08e4920f711bb94d291fb83bc7ea76986c380f2b
Charly Chevalier <charly.chevalier@consensys.net>
2025-03-24 15:28:08 +0100

    fix: fix `tsd-test.sh` + add top-level `test:types` rule (#262)
    Simplify and fix the `tsd-test.sh` wrapper again. Also fixing the typing
    tests that went unnoticed with the introduciont of `scopes`.

--- scripts/tsd-test.sh
+++ scripts/tsd-test.sh
@@ -9,17 +9,12 @@ if [[ $# -eq 0 ]]; then
 fi
 
 package_test_dir="$1"
+package_test_files=("${package_test_dir}"/**/*.test-d.ts)
 
-# For some reason, just running `tsd` with no arguments does not work properly. If you have some
-# static errors in your *.test-d.ts, they might not be evaluated. However, specifying each tests
-# with `--files` works everytime. So for now, we just use this wrapper for our typing tests.
-# NOTE: This directive is expected since we want the output to be splitted:
-# shellcheck disable=SC2046
-package_test_files="$(find "${package_test_dir}" -name "*.test-d.ts" -exec echo -n "--files {} " \;)"
-if [ "${package_test_files}" == "" ]; then
-  # If there's no files, we prevent from calling `tsd` with no arguments, otherwise it fallsback
-  # to the original behavior which is to test every `*d.ts` files.
-  echo "Nothing to test with tsd."
+# To avoid logging a tsd error message, we check if it exists any file for that
+# pattern:
+if [ -e "${package_test_files[0]}" ]; then
+  tsd --files "${package_test_dir}/**/*.test-d.ts"
 else
-  tsd "${package_test_files}"
+  echo "Nothing to test with tsd."
 fi
