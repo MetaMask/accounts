8d08f2e0bb8a3fe4b730bff1b7febc1516486766
Xiaoming Wang <dawnseeker8@gmail.com>
2025-06-09 12:46:49 +0100

    Improve error handling to handle TransportStatusError.

--- packages/keyring-eth-ledger-bridge/src/ledger-keyring.ts
+++ packages/keyring-eth-ledger-bridge/src/ledger-keyring.ts
@@ -5,6 +5,7 @@ import {
   type TypedTransaction,
 } from '@ethereumjs/tx';
 import { publicToAddress } from '@ethereumjs/util';
+import { TransportStatusError } from '@ledgerhq/hw-transport';
 import type { MessageTypes, TypedMessage } from '@metamask/eth-sig-util';
 import {
   recoverPersonalSignature,
@@ -424,27 +425,29 @@ export class LedgerKeyring implements Keyring {
         tx: remove0x(rawTxHex),
         hdPath,
       });
-    } catch (error) {
-      /**
-       * for user rejected the transaction error
-       */
-      if (
-        LedgerKeyring.#isLedgerError(error) &&
-        error.errorCode === '6985' &&
-        error.message === 'Condition not satisfied'
-      ) {
-        throw new Error('Ledger: User rejected the transaction');
+    } catch (error: unknown) {
+      // check whether error is TransportError
+      if (error instanceof TransportStatusError) {
+        // cast error to TransportError
+        const transportError: TransportStatusError = error;
+        if (
+          transportError.statusCode === 27013 &&
+          transportError.message.includes('(denied by the user?) (0x6985)')
+        ) {
+          throw new Error('Ledger: User rejected the transaction');
+        }
+
+        /**
+         * for user rejected the transaction error
+         */
+        if (
+          transportError.statusCode === 27013 &&
+          transportError.message.includes('(denied by the user?) (0x6985)')
+        ) {
+          throw new Error('Ledger: User rejected the transaction');
+        }
       }
-      /**
-       * for blind signing disabled error
-       */
-      if (
-        LedgerKeyring.#isLedgerError(error) &&
-        error.errorCode === '6a80' &&
-        error.message === 'Invalid data'
-      ) {
-        throw new Error('Ledger: Blind signing must be enabled');
-      }
+
       throw error instanceof Error
         ? error
         : new Error('Ledger: Unknown error while signing transaction');
@@ -479,7 +482,19 @@ export class LedgerKeyring implements Keyring {
         hdPath,
         message: remove0x(message),
       });
-    } catch (error) {
+    } catch (error: unknown) {
+      if (error instanceof TransportStatusError) {
+        const transportError: TransportStatusError = error;
+        /**
+         * for user rejected the transaction error
+         */
+        if (
+          transportError.statusCode === 27013 &&
+          transportError.message.includes('(denied by the user?) (0x6985)')
+        ) {
+          throw new Error('Ledger: User rejected the transaction');
+        }
+      }
       /**
        * for user rejected the transaction error
        */
@@ -582,26 +597,26 @@ export class LedgerKeyring implements Keyring {
           message,
         },
       });
-    } catch (error) {
-      /**
-       * for user rejected the transaction error
-       */
-      if (
-        LedgerKeyring.#isLedgerError(error) &&
-        error.errorCode === '6985' &&
-        error.message === 'Condition not satisfied'
-      ) {
-        throw new Error('Ledger: User rejected the transaction');
-      }
-      /**
-       * for blind signing disabled error
-       */
-      if (
-        LedgerKeyring.#isLedgerError(error) &&
-        error.errorCode === '6a80' &&
-        error.message === 'Invalid data'
-      ) {
-        throw new Error('Ledger: Blind signing must be enabled');
+    } catch (error: unknown) {
+      if (error instanceof TransportStatusError) {
+        // cast error to TransportError
+        const transportError: TransportStatusError = error;
+        if (
+          transportError.statusCode === 27013 &&
+          transportError.message.includes('(denied by the user?) (0x6985)')
+        ) {
+          throw new Error('Ledger: User rejected the transaction');
+        }
+
+        /**
+         * for user rejected the transaction error
+         */
+        if (
+          transportError.statusCode === 27013 &&
+          transportError.message.includes('(denied by the user?) (0x6985)')
+        ) {
+          throw new Error('Ledger: User rejected the transaction');
+        }
       }
       throw error instanceof Error
         ? error
