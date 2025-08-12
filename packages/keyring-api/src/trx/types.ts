39cf7a9475096814da35ffeca9646b579642c75e
Antonio Regadas <antonio.regadas@consensys.net>
2025-08-12 16:19:42 +0100

    chore: lint fix

--- packages/keyring-api/src/trx/types.ts
+++ packages/keyring-api/src/trx/types.ts
@@ -1,12 +1,11 @@
 import { object } from '@metamask/keyring-utils';
 import type { Infer } from '@metamask/superstruct';
-import { array, enums, literal, nonempty, record } from '@metamask/superstruct';
+import { array, enums, literal, nonempty } from '@metamask/superstruct';
 import { definePattern } from '@metamask/utils';
 
 import {
   CaipChainIdStruct,
   KeyringAccountStruct,
-  SolAccountType,
   TrxAccountType,
 } from '../api';
 
@@ -23,28 +22,28 @@ export const TrxAddressStruct = definePattern(
  * Supported TRON methods.
  */
 export declare enum TrxMethod {
-  SignMessageV2 = "signMessageV2",
-  VerifyMessageV2 = "verifyMessageV2"
+  SignMessageV2 = 'signMessageV2',
+  VerifyMessageV2 = 'verifyMessageV2',
 }
 
 export const TrxEoaAccountStruct = object({
-    ...KeyringAccountStruct.schema,
-    /**
-     * Account address.
-     */
-    address: TrxAddressStruct,
-    /**
-     * Account type.
-     */
-    type: literal(`${TrxAccountType.Eoa}`),
-    /**
-     * Account supported scopes (CAIP-2 chain IDs).
-     */
-    scopes: nonempty(array(CaipChainIdStruct)),
-    /**
-     * Account supported methods.
-     */
-    methods: array(enums(Object.values(TrxMethod))),
+  ...KeyringAccountStruct.schema,
+  /**
+   * Account address.
+   */
+  address: TrxAddressStruct,
+  /**
+   * Account type.
+   */
+  type: literal(`${TrxAccountType.Eoa}`),
+  /**
+   * Account supported scopes (CAIP-2 chain IDs).
+   */
+  scopes: nonempty(array(CaipChainIdStruct)),
+  /**
+   * Account supported methods.
+   */
+  methods: array(enums(Object.values(TrxMethod))),
 });
 
 export type TrxEoaAccount = Infer<typeof TrxEoaAccountStruct>;
