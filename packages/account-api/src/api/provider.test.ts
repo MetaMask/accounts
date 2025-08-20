2c749560b32927b33c7dd28269cd3ce98760e02e
Hassan Malik <hbmalik88@gmail.com>
2025-08-20 11:08:35 -0400

    fix: lint fix

--- packages/account-api/src/api/provider.test.ts
+++ packages/account-api/src/api/provider.test.ts
@@ -2,7 +2,7 @@ import { AccountProviderType } from './provider';
 
 describe('AccountProviderType', () => {
   it('has expected values', () => {
-    expect(AccountProviderType).toEqual({
+    expect(AccountProviderType).toStrictEqual({
       Evm: 'Evm',
       Solana: 'Solana',
       Btc: 'Btc',
