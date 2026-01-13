5d699ccd8a6d7c5024f2b0d8aeb9d9f13f3abbef
Charly Chevalier <charly.chevalier@consensys.net>
2026-01-13 22:07:09 +0100

    chore: fix lint

--- packages/hw-device-sdk/src/index.test.ts
+++ packages/hw-device-sdk/src/index.test.ts
@@ -1,3 +1,6 @@
+// TODO: Remove this once the package has actual exports.
+/* eslint-disable import-x/unambiguous */
+
 describe('index', () => {
   it('does nothing', () => {
     expect(true).toBe(true);
---
+++ packages/hw-device-sdk/src/index.ts
@@ -0,0 +1,2 @@
+// TODO: Remove this once the package has actual exports.
+/* eslint-disable import-x/unambiguous */
