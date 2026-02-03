ff2729c3c8b24f5c8d01b4c51f93813d18ac5875
Charly Chevalier <charly.chevalier@consensys.net>
2026-02-03 22:08:33 +0100

    fix: properly wrap options

--- packages/keyring-snap-client/src/KeyringClient.ts
+++ packages/keyring-snap-client/src/KeyringClient.ts
@@ -125,7 +125,7 @@ export class KeyringClient implements Keyring {
     return strictMask(
       await this.send({
         method: KeyringRpcMethod.CreateAccounts,
-        params: options,
+        params: { options },
       }),
       CreateAccountsResponseStruct,
     );
