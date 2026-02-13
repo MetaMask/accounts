26b059990b7c9a70dabd12a3c38d7fab0fa79bae
Matthias Geihs <matthias.geihs@consensys.net>
2026-02-13 12:51:33 +0100

    fixup

--- packages/keyring-eth-mpc/src/index.ts
+++ packages/keyring-eth-mpc/src/index.ts
@@ -2,7 +2,6 @@ export { MPCKeyring } from './mpc-keyring';
 export { uninitializedResponderState } from './mpc-keyring';
 
 export type {
-  InitRole,
   MPCKeyringOpts,
   MPCKeyringSerializer,
   ThresholdKeyId,
