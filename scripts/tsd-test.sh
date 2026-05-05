2596b5fed8e5292ab87fecdb63199b088cfe566a
Charly Chevalier <charly.chevalier@consensys.net>
2026-05-05 11:47:01 +0200

    fix: fix scripts/tsd-test.sh

--- scripts/tsd-test.sh
+++ scripts/tsd-test.sh
@@ -9,7 +9,7 @@ if [[ $# -eq 0 ]]; then
 fi
 
 package_test_dir="$1"
-package_test_files=("${package_test_dir}"/**/*.test-d.ts)
+package_test_files=($(find "${package_test_dir}" -name "*.test-d.ts"))
 
 # To avoid logging a tsd error message, we check if it exists any file for that
 # pattern:
