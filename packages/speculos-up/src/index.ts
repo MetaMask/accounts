aea97514fb25069ab431fa32172587dff7c3c8aa
Monte Lai <monte.lai@consensys.net>
2026-05-28 19:05:29 +0800

    fix: remove not used

--- packages/speculos-up/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { ensureBinary, getBinaryPath, type EnsureBinaryOptions } from './download';
-export { getPlatform } from './platform';
-export {
-  SPECULOS_VERSION,
-  RELEASE_BASE_URL,
-  DEFAULT_CACHE_DIR,
-  PLATFORMS,
-  type PlatformArch,
-} from './constants';
