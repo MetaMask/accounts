7c29631076ad4c6082aa3d2ebc04c199f968bdc2
Charly Chevalier <charly.chevalier@consensys.net>
2026-05-28 19:00:11 +0200

    fix: remove extra export

--- packages/keyring-snap-bridge/src/index.ts
+++ packages/keyring-snap-bridge/src/index.ts
@@ -1,6 +1,5 @@
 export * from './options';
 export * from './types';
 export * from './SnapKeyring';
-export * from './v2/SnapKeyringV1Adapter';
 export * from './SnapKeyringV1';
 export type * from './SnapKeyringMessenger';
