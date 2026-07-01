879b5f2efc6a3dc4a16e117c9982c1cb55688978
Hassan Malik <hbmalik88@gmail.com>
2026-07-01 15:30:48 +0200

    chore(keyring-snap-bridge): update comment

--- packages/keyring-snap-bridge/src/v2/SnapKeyring.ts
+++ packages/keyring-snap-bridge/src/v2/SnapKeyring.ts
@@ -558,9 +558,12 @@ export class SnapKeyring extends SnapKeyringV1 implements Keyring {
    */
   #resolveKeyringCapabilities(): KeyringCapabilities | undefined {
     const snap = this.messenger.call('SnapController:getSnap', this.snapId);
-    // we are not validating the shape of the capabilities here, because there is
+    // READ THIS CAREFULLY:
+    // We are not validating the shape of the capabilities here, because there is
     // manifest validation done already on the snaps side, the snaps repo maintains
-    // a copy of the KeyringCapabilitiesStruct
+    // a copy of the `KeyringCapabilitiesStruct`.
+    // We must ensure that both structs are always in-sync, otherwise the type-cast
+    // could cause runtime issues!
     return snap?.manifest.initialPermissions['endowment:keyring']
       ?.capabilities as KeyringCapabilities | undefined;
   }
