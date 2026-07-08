7db3065ee0d6deb847b927bfeb88897fed4e0835
Charly Chevalier <charly.chevalier@consensys.net>
2026-07-08 16:53:35 +0200

    feat: add PrivateKeyAccount type helper

---
+++ packages/account-api/src/api/private-key.ts
@@ -0,0 +1,43 @@
+import type {
+  KeyringAccount,
+  KeyringAccountEntropyPrivateKeyOptions,
+} from '@metamask/keyring-api';
+
+/**
+ * Private key compatible account type.
+ */
+export type PrivateKeyAccount<Account extends KeyringAccount> = Account & {
+  // We force the option type for those accounts. (That's how we identify
+  // if an account is private key compatible).
+  options: {
+    entropy: KeyringAccountEntropyPrivateKeyOptions;
+  };
+};
+
+/**
+ * Checks if an account is private key compatible.
+ *
+ * @param account - The account to be tested.
+ * @returns True if the account is private key compatible.
+ */
+export function isPrivateKeyAccount<Account extends KeyringAccount>(
+  account: Account,
+): account is PrivateKeyAccount<Account> {
+  // To be private key compatible, we just check for the entropy type (the
+  // the `entropy` shape will be inferred automatically).
+  return account.options.entropy?.type === 'private-key';
+}
+
+/**
+ * Asserts a keyring account is private keycompatible.
+ *
+ * @param account - Keyring account to check.
+ * @throws If the keyring account is not compatible.
+ */
+export function assertIsPrivateKeyAccount<Account extends KeyringAccount>(
+  account: Account,
+): asserts account is PrivateKeyAccount<Account> {
+  if (!isPrivateKeyAccount(account)) {
+    throw new Error('Account is not private key compatible');
+  }
+}
