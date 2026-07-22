abb5113f2c124d21648e6a72bc2dcc100585b4f5
Charly Chevalier <charly.chevalier@consensys.net>
2026-07-22 16:00:57 +0200

    refactor: re-use BalanceStruct in FungibleAssetAmountStruct

--- packages/keyring-api/src/api/balance.ts
+++ packages/keyring-api/src/api/balance.ts
@@ -4,8 +4,19 @@ import { exactOptional, object, record, string } from '@metamask/superstruct';
 import { JsonStruct } from '@metamask/utils';
 
 export const BalanceStruct = object({
+  /**
+   * Asset amount.
+   */
   amount: StringNumberStruct,
+
+  /**
+   * Asset unit.
+   */
   unit: string(),
+
+  /**
+   * Optional arbitrary metadata associated with this balance.
+   */
   metadata: exactOptional(record(string(), JsonStruct)),
 });
 
