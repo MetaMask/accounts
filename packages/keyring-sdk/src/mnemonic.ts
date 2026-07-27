750b9129ea7eaa192f63dbeb0becdde395e188bd
Charly Chevalier <charly.chevalier@consensys.net>
2026-07-27 14:07:49 +0200

    feat(keyring-sdk): add encodeMnemonicWords

--- packages/keyring-sdk/src/mnemonic.ts
+++ packages/keyring-sdk/src/mnemonic.ts
@@ -1,18 +1,27 @@
 import { wordlist } from '@metamask/scure-bip39/dist/wordlists/english';
 
 /**
- * Encodes a mnemonic as an array of bytes (UTF-8).
+ * Encodes a mnemonic as a string of words.
  *
  * @param mnemonicIndicesBytes - An array of bytes (16-bit unsigned integers) representing the indices of the words in the mnemonic.
- * @returns An array of bytes (UTF-8) representing the mnemonic.
+ * @returns A string representing the mnemonic.
  */
-export function encodeMnemonic(mnemonicIndicesBytes: Uint8Array): number[] {
+export function encodeMnemonicWords(mnemonicIndicesBytes: Uint8Array): string {
   const mnemonicIndices = Array.from(
     // Create a new `Uint8Array` to ensure we have a proper view on the buffer
     // without having to worry about `byteOffset` and `byteLength` of
     // the inner buffer.
     new Uint16Array(new Uint8Array(mnemonicIndicesBytes).buffer),
   );
-  const mnemonic = mnemonicIndices.map((i) => wordlist[i]).join(' ');
-  return Array.from(new TextEncoder().encode(mnemonic));
+  return mnemonicIndices.map((i) => wordlist[i]).join(' ');
+}
+
+/**
+ * Encodes a mnemonic as an array of bytes (UTF-8).
+ *
+ * @param mnemonicIndicesBytes - An array of bytes (16-bit unsigned integers) representing the indices of the words in the mnemonic.
+ * @returns An array of bytes (UTF-8) representing the mnemonic.
+ */
+export function encodeMnemonic(mnemonicIndicesBytes: Uint8Array): number[] {
+  return Array.from(new TextEncoder().encode(encodeMnemonicWords(mnemonicIndicesBytes)));
 }
