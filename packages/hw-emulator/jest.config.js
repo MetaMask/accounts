1d86c0e105d2bb8a9c9952ef397020cda5f60dec
Monte Lai <monte.lai@consensys.net>
2026-08-18 21:34:00 +0800

    fix(hw-emulator): exclude spike dirs from jest test discovery
    Spike browser scripts named test.js are picked up by jest's default
    testMatch and fail with 'document is not defined' in the node
    environment.

--- packages/hw-emulator/jest.config.js
+++ packages/hw-emulator/jest.config.js
@@ -4,7 +4,9 @@ const baseConfig = require('../../jest.config.packages');
 
 module.exports = merge(baseConfig, {
   displayName: path.basename(__dirname),
-  testPathIgnorePatterns: ['/node_modules/'],
+  // Spike directories contain browser-context scripts named `test.js` that
+  // Jest's default testMatch would otherwise pick up as test suites.
+  testPathIgnorePatterns: ['/node_modules/', '/spike/'],
   coverageThreshold: {
     global: { branches: 10, functions: 20, lines: 20, statements: 20 },
   },
